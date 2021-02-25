e1a13372c9d43c8c0b2b1f1eb76756060e948932
evanezcent <pratamayo16@gmail.com>
2021-02-25 10:17:40 +0700

    sign out google

--- lib/after.dart
+++ lib/after.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:social_login/user.dart'; 
+import 'package:social_login/handler.dart';
+import 'package:social_login/user.dart';
 
 class AfterLogin extends StatefulWidget {
   const AfterLogin({Key key, this.user}) : super(key: key);
@@ -14,15 +15,33 @@ class _AfterLoginState extends State<AfterLogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("NAMA ${widget.user.name}"),
-          Text("Email ${widget.user.email}"),
-          CircleAvatar(
-            backgroundColor: Colors.teal[800],
-            radius: 50,
-          )
-        ],
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("NAMA ${widget.user.name}"),
+            Text("Email ${widget.user.email}"),
+            CircleAvatar(
+              backgroundColor: Colors.teal[800],
+              radius: 50,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              onPressed: () {
+                HandlerFunction().signOutGoogle();
+                Navigator.pop(context);
+              },
+              color: Colors.blue,
+              child: Center(
+                child: Text("G-Sign Out"),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
